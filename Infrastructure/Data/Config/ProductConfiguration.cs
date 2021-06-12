a312c069dcd8fe1ab14771e668897d438f04a770
Mahdi <Mahdi@DESKTOP-DIL9O74>
2021-06-12 18:34:19 +0430

    End Of Section 3

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder) /*bayad implenment beshe*/
+        {
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired(); /*age khasti HasMaxLength() bezari ye meghdari bede to seed data be moshkel nakhori. ya aslan ese alan nanevis*/
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)"); /*chon sql server va sqlite decimal support nemikone*/
+            builder.Property(p => p.PictureUrl).IsRequired();
+            builder.HasOne(b => b.ProductBrand).WithMany()
+                .HasForeignKey(p => p.ProductBrandId); /*in khat bara relation bein jadavele*/
+            builder.HasOne(t => t.ProductType).WithMany()
+                .HasForeignKey(p => p.ProductTypeId); /*in khat bara relation bein jadavele*/
+        }
+    } /*ghablana in tanzimato dakhel OnModelCreating dakhel file Context gharar midadim. in ravesh jadide*/
+}
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
@@ -14,5 +15,13 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Product> Products { get; set; }
-    }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        } /*ghablan to in bakhsh Tanzimat, configure va relationship haye database anjam mishod. alan dakhel file config to Infrastructure bara har Entity be soorat mojaza tarif shode*/
+    } /*bade etmam configuration ha yek bar file Migrations remove mishe bad az no sakhte mishe*/
 }
