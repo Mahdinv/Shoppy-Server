0ad56a1d722708b121a1c2a9f688b02c1144c535
Mahdi <Mahdi@DESKTOP-DIL9O74>
2021-07-06 19:49:12 +0430

    End Of Section 10

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailsComponent } from './shop/product-details/product-details.component';
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  // {path: 'shop', component: ShopComponent},
+  // {path: 'shop/:id', component: ProductDetailsComponent},
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)}, /*tozihat safe 5 mored 6 */
+  {path: '**', redirectTo: '', pathMatch: 'full'} /*bere be home component */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
